0d7b4a0acc374bc4e83f0626c190a98c3d115035
riddhi <riddhi@matrixbricks.com>
2025-03-28 17:56:25 +0530

    Dashboard chnages along with login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
-Route::get('/', function () {
-    return view('welcome');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/login', [LoginController::class, 'login'])->name('admin.login');
+Route::post('/login', [LoginController::class, 'authenticate'])->name('admin.authenticate');
+Route::get('/logout', [LoginController::class, 'logout'])->name('admin.logout');
+Route::get('/change-password', [LoginController::class, 'change_password'])->name('admin.changepassword');
+Route::post('/update-password', [LoginController::class, 'updatePassword'])->name('admin.updatepassword');
+
+
+// Admin Routes with Middleware
+Route::group(['middleware' => ['auth:web', \App\Http\Middleware\PreventBackHistoryMiddleware::class]], function () {
+    Route::get('/dashboard', function () {
+        return view('backend.dashboard'); 
+    })->name('admin.dashboard');
 });
